da44f7a0dcb338f38628cc228724b88e17bb1872
StefanIntertec <stefan.markovikj@intertec.io>
2022-11-09 14:45:06 +0100

    Add SwiftUI SDK

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# visx-swiftui-sdk-ios
+# visx-sdk-ios
+
+VIS.X® is a new and unique kind of advertising technology that enables efficient execution of media and high-impact ad products at scale. VIS.X® wraps the inventory in a holistic auction offering all available products in one transaction to buyers, consequently optimizing the bidstream. YOC has developed this platform to unlock the real value of digital advertising – making VIS.X® the go-to-platform for high-impact programmatic advertising.
---
+++ VisxSwiftUISDK.xcframework/ios-x86_64-simulator/VisxSwiftUISDK.framework/Headers/VisxSwiftUISDK.h
@@ -0,0 +1,18 @@
+//
+//  VisxSwiftUISDK.h
+//  VisxSwiftUISDK
+//
+//  Created by Stefan Markovikj on 22.7.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VisxSwiftUISDK.
+FOUNDATION_EXPORT double VisxSwiftUISDKVersionNumber;
+
+//! Project version string for VisxSwiftUISDK.
+FOUNDATION_EXPORT const unsigned char VisxSwiftUISDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VisxSwiftUISDK/PublicHeader.h>
+
+
